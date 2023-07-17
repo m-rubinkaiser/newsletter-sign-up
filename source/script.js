f81e04c8a82a914b75760977f63059bd814d49da
Your Rubin <m.rubinkaiser@gmail.com>
2023-07-17 19:42:18 +0530

    update

--- source/script.js
+++ source/script.js
@@ -1,23 +1,21 @@
-let signUp = document.getElementById("sign-up");
-
 function sign() {
     let success = document.getElementById("success");
+    var email1 = document.getElementById("email");
     var email = document.getElementById("email").value;
     let done = document.getElementById("done");
     if (
-        email == "" ||
         !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
     ) {
         let e = "valid email required";
         document.getElementById("error").innerHTML = e;
+        email1.style.background = " hsl(4, 100%, 67%)";
     } else {
-        success.style.display = "block";
+        success.style.display = "grid";
         done.style.display ="none";
     }
 }
 
 function resetError() {
-    console.log("asdf");
     document.getElementById("error").innerHTML = "";
 }
 
