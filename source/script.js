b97420b3f1cc0912b76d21bcac338806c96a7bc3
Your Rubin <m.rubinkaiser@gmail.com>
2023-07-18 20:44:20 +0530

    update

--- source/script.js
+++ source/script.js
@@ -8,11 +8,17 @@ function sign() {
     ) {
         let e = "valid email required";
         document.getElementById("error").innerHTML = e;
-        email1.style.background = " hsl(4, 100%, 67%)";
+        email1.style.background = " rgba(255,232,230,1)";
+        email1.style.color="red";
     } else {
         success.style.display = "grid";
         done.style.display ="none";
     }
+}
+function gmail(){
+    var email1 = document.getElementById("email");
+        email1.style.color="black";
+        email1.style.background = " white";
 }
 
 function resetError() {
